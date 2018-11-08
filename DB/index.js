461195961b2c68d4d0c1a5e15491825f2dd9d18c
unknown <tmsushil@gmail.com>
2018-11-08 20:09:35 +0800

    updated few functionalities

--- DB/index.js
+++
@@ -1,8 +0,0 @@
-import Sequelize from 'sequelize';
-import DB from './connection';
-
-const initializeDB = () => DB.sync();
-
-export const closeDB = () => DB.close();
-
-export default initializeDB;
